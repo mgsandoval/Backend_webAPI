1ea3fc19a04020983c7e04e52018477a5480095d
mgsandoval <gabrielsandoval06@hotmail.com>
2025-03-12 10:36:45 -0600

    Agregar archivos de proyecto.

---
+++ Backend_webAPI/Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Backend_webAPI.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
