9c1d311e0251b800cb06caf70df31eef57e77246
mgsandoval <gabrielsandoval06@hotmail.com>
2025-03-12 15:31:33 -0600

    Eliminado el proyecto sin MVC y detalles por corregir: input Submit "Subir archivo Excel" no funciona.

--- Backend_webAPI_ASP.NETCore/Controllers/HomeController.cs
+++ Backend_webAPI_ASP.NETCore/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Backend_webAPI_ASP.NETCore.Models;
+using ExcelDataReader;
 
 namespace Backend_webAPI_ASP.NETCore.Controllers;
 
@@ -37,13 +38,49 @@ public class HomeController : Controller
     [HttpPost]
     public async Task<IActionResult> ExcelFileReader(IFormFile file)
     {
-        if (file == null || file.Length <= 0)
-        {
-            var uploadDirectory = $"{Directory.GetCurrentDirectory()}\\wwwroot\\uploads";
+        System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
 
+        // Upload file to server
+        // string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Uploads");
+        string path = Directory.GetCurrentDirectory();
+        Console.WriteLine(path);
+        if (file != null && file.Length > 0)
+        {
+            var uploadDirectory = $"{Directory.GetCurrentDirectory()}\\Uploads";
+             
             if (!Directory.Exists(uploadDirectory))
             {
+                Console.WriteLine("Directorio inexistente");
                 Directory.CreateDirectory(uploadDirectory);
+            }
+
+            var filePath = Path.Combine(uploadDirectory, file.FileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            // Read file
+            using (var stream = System.IO.File.Open(filePath, FileMode.Open, FileAccess.Read))
+            {
+                var excelData = new List<List<object>>();
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    do
+                    {
+                        while (reader.Read())
+                        {
+                            var rowData = new List<object>();
+                            for (int column =0; column < reader.FieldCount; column++)
+                            {
+                                rowData.Add(reader.GetValue(column));
+                            }
+                            excelData.Add(rowData);
+                        }
+                    } while (reader.NextResult());
+                    ViewBag.ExcelData = excelData;
+                }
             }
         }
         return View();
