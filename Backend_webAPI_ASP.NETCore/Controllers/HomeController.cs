d5e105e965f4acf01969850c05f39fb1d446f8bd
mgsandoval <gabrielsandoval06@hotmail.com>
2025-03-17 09:00:26 -0600

    Codificando la conexión a SAP B1 (contiene errores).

--- Backend_webAPI_ASP.NETCore/Controllers/HomeController.cs
+++ Backend_webAPI_ASP.NETCore/Controllers/HomeController.cs
@@ -6,11 +6,18 @@ using NPOI.XSSF.UserModel;
 using NPOI.HSSF.UserModel;
 using Microsoft.EntityFrameworkCore;
 using Backend_webAPI_ASP.NETCore.Models.ViewModels;
+using SAPbobsCOM;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Backend_webAPI_ASP.NETCore.Controllers
 {
     public class HomeController : Controller
     {
+        private SAPbobsCOM.Company myCompany;
+        private SAPbobsCOM.BusinessPartners mySN;
+        private int err;
+        private string errMsg;
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -50,80 +57,156 @@ namespace Backend_webAPI_ASP.NETCore.Controllers
                 IRow fila = HojaExcel.GetRow(i);
                 lista.Add(new VMContacto
                 {
-                    Codigo                  = fila.GetCell(0) .ToString(),
-                    Nombre_razon_social     = fila.GetCell(1) .ToString(),
-                    Tipo_cliente            = fila.GetCell(2) .ToString(),
-                    Moneda                  = fila.GetCell(3) .ToString(),
-                    Telefono1               = fila.GetCell(4) .ToString(),
-                    Telefono_movil          = fila.GetCell(5) .ToString(),
-                    Correo_electronico      = fila.GetCell(6) .ToString(),
-                    RTN                     = fila.GetCell(7) .ToString(),
-                    Direccion               = fila.GetCell(8) .ToString(),
-                    Vendedor                = fila.GetCell(9) .ToString(),
-                    Territorio              = fila.GetCell(10).ToString(),
-                    Nombre_completo         = fila.GetCell(11).ToString(),
-                    Nombre                  = fila.GetCell(12).ToString(),
-                    Apellido                = fila.GetCell(13).ToString(),
-                    Telefono_fijo           = fila.GetCell(14).ToString(),
-                    Movil_personal          = fila.GetCell(15).ToString(),
-                    Correo_electronico2     = fila.GetCell(16).ToString(),
-                    Destino                 = fila.GetCell(17).ToString(),
-                    Id_direccion            = fila.GetCell(18).ToString(),
-                    Nombre_direccion2       = fila.GetCell(19).ToString(),
-                    Nombre_direccion3       = fila.GetCell(20).ToString(),
-                    Ciudad                  = fila.GetCell(21).ToString(),
-                    Condado                 = fila.GetCell(22).ToString(),
-                    Condiciones_pago        = fila.GetCell(23).ToString(),
-                    Lista_precios           = fila.GetCell(24).ToString(),
-                    Limite_credito          = fila.GetCell(25).ToString(),
-                    Cuenta_mayor_sucursal   = fila.GetCell(26).ToString()
+                    Codigo = fila.GetCell(0).ToString(),
+                    Nombre_razon_social = fila.GetCell(1).ToString(),
+                    Tipo_cliente = fila.GetCell(2).ToString(),
+                    Moneda = fila.GetCell(3).ToString(),
+                    Telefono1 = fila.GetCell(4).ToString(),
+                    Telefono_movil = fila.GetCell(5).ToString(),
+                    Correo_electronico = fila.GetCell(6).ToString(),
+                    RTN = fila.GetCell(7).ToString(),
+                    Direccion = fila.GetCell(8).ToString(),
+                    Vendedor = fila.GetCell(9).ToString(),
+                    Territorio = fila.GetCell(10).ToString(),
+                    Nombre_completo = fila.GetCell(11).ToString(),
+                    Nombre = fila.GetCell(12).ToString(),
+                    Apellido = fila.GetCell(13).ToString(),
+                    Telefono_fijo = fila.GetCell(14).ToString(),
+                    Movil_personal = fila.GetCell(15).ToString(),
+                    Correo_electronico2 = fila.GetCell(16).ToString(),
+                    Destino = fila.GetCell(17).ToString(),
+                    Id_direccion = fila.GetCell(18).ToString(),
+                    Nombre_direccion2 = fila.GetCell(19).ToString(),
+                    Nombre_direccion3 = fila.GetCell(20).ToString(),
+                    Ciudad = fila.GetCell(21).ToString(),
+                    Condado = fila.GetCell(22).ToString(),
+                    Condiciones_pago = fila.GetCell(23).ToString(),
+                    Lista_precios = fila.GetCell(24).ToString(),
+                    Limite_credito = fila.GetCell(25).ToString(),
+                    Cuenta_mayor_sucursal = fila.GetCell(26).ToString()
                 });
             }
 
             return StatusCode(StatusCodes.Status200OK, lista);
         }
 
-        //[HttpPost]
-        //public IActionResult EnviarDatos([FromForm] IFormFile ArchivoExcel)
-        //{
-        //    Stream stream = ArchivoExcel.OpenReadStream();
 
-        //    IWorkbook MiExcel = null;
+        [HttpPost]
+        public IActionResult EnviarDatos([FromForm] IFormFile ArchivoExcel)
+        {
+            Stream stream = ArchivoExcel.OpenReadStream();
+            IWorkbook MiExcel = null;
 
-        //    if (Path.GetExtension(ArchivoExcel.FileName) == ".xlsx")
-        //    {
-        //        MiExcel = new XSSFWorkbook(stream);
-        //    }
-        //    else
-        //    {
-        //        MiExcel = new HSSFWorkbook(stream);
-        //    }
+            if (Path.GetExtension(ArchivoExcel.FileName) == ".xlsx")
+            {
+                MiExcel = new XSSFWorkbook(stream);
+            }
+            else
+            {
+                MiExcel = new HSSFWorkbook(stream);
+            }
 
-        //    ISheet HojaExcel = MiExcel.GetSheetAt(0);
+            ISheet HojaExcel = MiExcel.GetSheetAt(0);
+            int cantidadFilas = HojaExcel.LastRowNum;
+            List<Contacto> lista = new List<Contacto>();
 
-        //    int cantidadFilas = HojaExcel.LastRowNum;
+            for (int i = 1; i <= cantidadFilas; i++)
+            {
+                IRow fila = HojaExcel.GetRow(i);
+                lista.Add(new Contacto
+                {
+                    Codigo = fila.GetCell(0).ToString(),
+                    Nombre_razon_social = fila.GetCell(1).ToString(),
+                    Tipo_cliente = fila.GetCell(2).ToString(),
+                    Moneda = fila.GetCell(3).ToString(),
+                    Telefono1 = fila.GetCell(4).ToString(),
+                    Telefono_movil = fila.GetCell(5).ToString(),
+                    Correo_electronico = fila.GetCell(6).ToString(),
+                    RTN = fila.GetCell(7).ToString(),
+                    Direccion = fila.GetCell(8).ToString(),
+                    Vendedor = fila.GetCell(9).ToString(),
+                    Territorio = fila.GetCell(10).ToString(),
+                    Nombre_completo = fila.GetCell(11).ToString(),
+                    Nombre = fila.GetCell(12).ToString(),
+                    Apellido = fila.GetCell(13).ToString(),
+                    Telefono_fijo = fila.GetCell(14).ToString(),
+                    Movil_personal = fila.GetCell(15).ToString(),
+                    Correo_electronico2 = fila.GetCell(16).ToString(),
+                    Destino = fila.GetCell(17).ToString(),
+                    Id_direccion = fila.GetCell(18).ToString(),
+                    Nombre_direccion2 = fila.GetCell(19).ToString(),
+                    Nombre_direccion3 = fila.GetCell(20).ToString(),
+                    Ciudad = fila.GetCell(21).ToString(),
+                    Condado = fila.GetCell(22).ToString(),
+                    Condiciones_pago = fila.GetCell(23).ToString(),
+                    Lista_precios = fila.GetCell(24).ToString(),
+                    Limite_credito = fila.GetCell(25).ToString(),
+                    Cuenta_mayor_sucursal = fila.GetCell(26).ToString()
+                });
+            }
 
-        //    List<Contacto> lista = new List<Contacto>();
+            // Conexión a SAP B1
+            try
+            {
+                Console.WriteLine("Intentando conectarse a SAP B1...");
+                Company myCompany = new Company();
+                myCompany.SLDServer = "192.168.100.241"; // IP o nombre de servidor
 
-        //    for (int i = 1; i <= cantidadFilas; i++)
-        //    {
+                myCompany.DbServerType = SAPbobsCOM.BoDataServerTypes.dst_MSSQL2012; // Tipo de servidor de base de datos (MSSQL, 2012, 2014, 2016, HANA)
+                myCompany.Server = "192.168.100.241"; // Nombre de la instancia de SQL Server
+                myCompany.CompanyDB = "PRUEBAS"; // Nombre de la base de datos
+                myCompany.UserName = "dev"; // Usuario de SAP B1
+                myCompany.Password = "Admin123"; // Contraseña de SAP B1
+                myCompany.language = SAPbobsCOM.BoSuppLangs.ln_Spanish_La; // Lenguaje de la base de datos, en este caso español de Latinoamérica
 
-        //        IRow fila = HojaExcel.GetRow(i);
+                err = myCompany.Connect();
 
-        //        lista.Add(new Contacto
-        //        {
-        //            Nombre = fila.GetCell(0).ToString(),
-        //            Apellido = fila.GetCell(1).ToString(),
-        //            Telefono = fila.GetCell(2).ToString(),
-        //            Correo = fila.GetCell(3).ToString(),
+                if (err != 0)
+                {
+                    Console.WriteLine("Error de conexión: " + myCompany.GetLastErrorDescription());
+                    //myCompany.GetLastError(out err, out errMsg);
+                    //return StatusCode(StatusCodes.Status500InternalServerError, new { mensaje = errMsg });
+                }
+                else
+                {
+                    Console.WriteLine("Conectado a SAP B1 exitosamente");
+                    // aquí deben ir las operaciones desde DI
+                    // por ejemplo, creación de clientes, facturas...
+                    mySN = (SAPbobsCOM.BusinessPartners)myCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.oBusinessPartners); // Se crea una instancia de la clase BusinessPartners de la DI API
+                    mySN.CardCode = "C0001"; // Código del cliente
+                    mySN.CardName = "Cliente creado desde DI API 01"; // Nombre del cliente
+                    mySN.AdditionalID = "CF"; // Tipo de identificación adicional
+                    mySN.FederalTaxID = "000000000000"; // Identificación fiscal
+                    mySN.Phone1 = "2255"; // Teléfono
 
-        //        });
-        //    }
+                    // Se intenta agregar el cliente a la base de datos
+                    err = mySN.Add();
 
-        //    _dbocontext.BulkInsert(lista);
+                    if (err != 0)
+                    {
+                        Console.WriteLine("Error al crear el cliente: " + myCompany.GetLastErrorDescription(), "Error");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Cliente creado exitosamente");
+                    }
 
-        //    return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok" });
-        //}
+
+                    if (myCompany.Connected == true)
+                    {
+                        myCompany.Disconnect();
+                        Console.WriteLine("SAP B1 ha sido desconectado");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error general al conectar a SAP B1: " + ex.Message);
+            }
+
+            return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok" });
+        }
+
 
 
         public IActionResult Privacy()
@@ -134,7 +217,7 @@ namespace Backend_webAPI_ASP.NETCore.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel { RequestId = System.Diagnostics.Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
